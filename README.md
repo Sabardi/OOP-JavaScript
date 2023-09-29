b6b001c09769b935f7859eae066c38601e389af7
Sabardi <Sabardibahari@gmail.com>
2023-09-30 02:30:08 +0800

    perkenalan oop

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Object-Oriented Programming
 ---------------------------
 Object-Oriented Programming (OOP) adalah salah satu paradigma dalam pemrograman yang berfokus pada pembuatan sebuah objek
 * Properti merupakan nilai di dalam objek yang menyimpan informasi tentang objek tersebut.
-* Method merupakan fungsi yang menggambarkan aksi yang dapat dilakukan oleh objek tersebut.
+* Method merupakan fungsi yang menggambarkan aksi yang dapat dilakukan oleh objek tersebut. 
--- class.js
+++ class.js
@@ -1,1 +1,27 @@
-console.log("first commit")
+const mobil = {
+    //property
+    brand : "honda",
+    warna : "red",
+    maxsped : 150,
+    DR : "DR 5678 HG",
+
+    //method
+    pengendara : ()=>{
+        console.log("pengendara")
+    },
+    putarbalik: () => {
+        console.log('putarbalik');
+      },
+      belok: () => {
+        console.log('belok')
+      }
+}
+
+console.log(mobil.brand);
+console.log(mobil.warna);
+console.log(mobil.maxsped);
+console.log(mobil.DR);
+
+mobil.pengendara()
+mobil.putarbalik()
+mobil.belok()
